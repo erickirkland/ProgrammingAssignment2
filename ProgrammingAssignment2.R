f8cadbffb6855a70914904128cdd08da7dadd796
Eric Kirkland <eric_kirkland@yahoo.com>
2014-08-24 00:00:42 -0400

    Add cachematrix.R

---
+++ ProgrammingAssignment2.R
@@ -0,0 +1,51 @@
+
+
+makeCacheMatrix <- function(x = matrix()) {
+
+    m <- NULL
+
+    set <- function(y){
+
+        x <<- y
+
+        m <<- NULL
+
+    }
+
+    get <- function() x
+
+    setmatrix <- function(solve) m <<- solve
+
+    getmatrix <- function() m
+
+    list(set = set, get = get,
+
+       setmatrix = setmatrix,
+
+       getmatrix = getmatrix)
+
+}
+
+
+
+cacheSolve <- function(x=matrix(), ...) {
+
+    m <- x$getmatrix()
+
+    if(!is.null(m)) {
+
+        message("getting cached matrix data")
+
+        return(m)
+
+    }
+
+    datamatrix <- x$get()
+
+    m <- solve(datamatrix, ...)
+
+    x$setmatrix(m)
+
+    m
+
+}
--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions work together to optimize performance
+## handling matrices. With large matrices that are not
+## changing, this technique eliminates reptitive processing.
+## The functions also demonstrate lexical scope.
 
-## Write a short comment describing this function
+## The function makeCacheMatrix takes a matrix as a parameter.
+## It solves the matrix and stores it in the parent scope.
+
+
 
 makeCacheMatrix <- function(x = matrix()) {
 
+    m <- NULL
+
+    set <- function(y){
+
+        x <<- y
+
+        m <<- NULL
+
+    }
+
+    get <- function() x
+
+    setmatrix <- function(solve) m <<- solve
+
+    getmatrix <- function() m
+
+    list(set = set, get = get,
+
+       setmatrix = setmatrix,
+
+       getmatrix = getmatrix)
+
 }
 
+## The function cacheSolve cooperaters with makeCacheMatrix.
+## If the matrix has been solved previously, which is
+## tested by checking the list "m" for NULL, it posts
+## the message and returns the solved matrix.  Otherwise
+## it solves the matrix.
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+cacheSolve <- function(x=matrix(), ...) {
+
+    m <- x$getmatrix()
+
+    if(!is.null(m)) {
+
+        message("getting cached matrix data")
+
+        return(m)
+
+    }
+
+    datamatrix <- x$get()
+
+    m <- solve(datamatrix, ...)
+
+    x$setmatrix(m)
+
+    m
+
 }
